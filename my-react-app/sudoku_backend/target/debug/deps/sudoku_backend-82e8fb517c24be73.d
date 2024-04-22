9a12ed3f526007f5505efdcae0b5db903e840aae
emmanuella <tekumnjit@gmail.com>
2024-04-22 10:27:49 +0100

    my react and rust working directory

---
+++ my-react-app/sudoku_backend/target/debug/deps/sudoku_backend-82e8fb517c24be73.d
@@ -0,0 +1,5 @@
+/home/gis/player/sudoku_puzzle/sudoku_backend/target/debug/deps/libsudoku_backend-82e8fb517c24be73.rmeta: src/main.rs
+
+/home/gis/player/sudoku_puzzle/sudoku_backend/target/debug/deps/sudoku_backend-82e8fb517c24be73.d: src/main.rs
+
+src/main.rs:
---
+++ my-react-app/sudoku_backend/target/debug/deps/sudoku_backend-d8116e04e0d04511.d
@@ -0,0 +1,5 @@
+/home/gis/player/sudoku_puzzle/sudoku_backend/target/debug/deps/libsudoku_backend-d8116e04e0d04511.rmeta: src/main.rs
+
+/home/gis/player/sudoku_puzzle/sudoku_backend/target/debug/deps/sudoku_backend-d8116e04e0d04511.d: src/main.rs
+
+src/main.rs:
